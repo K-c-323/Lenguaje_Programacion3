bda6fd40f188781cfd434d9c8786075832030c23
K-c-323 <99497355+K-c-323@users.noreply.github.com>
2022-02-13 08:47:26 -0600

    XD

--- Kevin_caceres_20191000816_evalucion_2/Kevin_caceres_20191000816_evalucion_2/Form1.Designer.cs
+++ Kevin_caceres_20191000816_evalucion_2/Kevin_caceres_20191000816_evalucion_2/Form1.Designer.cs
@@ -28,13 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombretextBox1 = new System.Windows.Forms.TextBox();
+            this.NacimientodateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.TrabajodateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Procesarbutton1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SueldotextBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PromedioVentastextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(56, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prueba si puedes Obtener un aumento !!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre :";
+            // 
+            // NombretextBox1
+            // 
+            this.NombretextBox1.Location = new System.Drawing.Point(175, 90);
+            this.NombretextBox1.Name = "NombretextBox1";
+            this.NombretextBox1.Size = new System.Drawing.Size(119, 20);
+            this.NombretextBox1.TabIndex = 2;
+            this.NombretextBox1.TextChanged += new System.EventHandler(this.NombretextBox1_TextChanged);
+            // 
+            // NacimientodateTimePicker1
+            // 
+            this.NacimientodateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.NacimientodateTimePicker1.Location = new System.Drawing.Point(175, 139);
+            this.NacimientodateTimePicker1.Name = "NacimientodateTimePicker1";
+            this.NacimientodateTimePicker1.Size = new System.Drawing.Size(136, 20);
+            this.NacimientodateTimePicker1.TabIndex = 3;
+            // 
+            // TrabajodateTimePicker2
+            // 
+            this.TrabajodateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.TrabajodateTimePicker2.Location = new System.Drawing.Point(175, 186);
+            this.TrabajodateTimePicker2.Name = "TrabajodateTimePicker2";
+            this.TrabajodateTimePicker2.Size = new System.Drawing.Size(136, 20);
+            this.TrabajodateTimePicker2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha de Nacimiento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fecha de Trabajo:";
+            // 
+            // Procesarbutton1
+            // 
+            this.Procesarbutton1.Location = new System.Drawing.Point(408, 111);
+            this.Procesarbutton1.Name = "Procesarbutton1";
+            this.Procesarbutton1.Size = new System.Drawing.Size(86, 28);
+            this.Procesarbutton1.TabIndex = 7;
+            this.Procesarbutton1.Text = "Procesar";
+            this.Procesarbutton1.UseVisualStyleBackColor = true;
+            this.Procesarbutton1.Click += new System.EventHandler(this.Procesarbutton1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sueldo : ";
+            // 
+            // SueldotextBox1
+            // 
+            this.SueldotextBox1.Location = new System.Drawing.Point(175, 297);
+            this.SueldotextBox1.Name = "SueldotextBox1";
+            this.SueldotextBox1.Size = new System.Drawing.Size(119, 20);
+            this.SueldotextBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Promedio de ventas :";
+            // 
+            // PromedioVentastextBox1
+            // 
+            this.PromedioVentastextBox1.Location = new System.Drawing.Point(175, 243);
+            this.PromedioVentastextBox1.Name = "PromedioVentastextBox1";
+            this.PromedioVentastextBox1.Size = new System.Drawing.Size(119, 20);
+            this.PromedioVentastextBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(512, 495);
+            this.Controls.Add(this.PromedioVentastextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.SueldotextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Procesarbutton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TrabajodateTimePicker2);
+            this.Controls.Add(this.NacimientodateTimePicker1);
+            this.Controls.Add(this.NombretextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombretextBox1;
+        private System.Windows.Forms.DateTimePicker NacimientodateTimePicker1;
+        private System.Windows.Forms.DateTimePicker TrabajodateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Procesarbutton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SueldotextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PromedioVentastextBox1;
     }
 }
 
--- Kevin_caceres_20191000816_evalucion_2/Kevin_caceres_20191000816_evalucion_2/Form1.cs
+++ Kevin_caceres_20191000816_evalucion_2/Kevin_caceres_20191000816_evalucion_2/Form1.cs
@@ -12,9 +12,131 @@ namespace Kevin_caceres_20191000816_evalucion_2
 {
     public partial class Form1 : Form
     {
+
+        
+
+
         public Form1()
         {
             InitializeComponent();
+
         }
+
+        private void NombretextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Procesarbutton1_Click(object sender, EventArgs e)
+        {
+            Boolean edadvalidacion;
+            Boolean Antiguedadvalidacion;
+            Boolean ventasvalidacion;
+            DateTime fechanacimien = NacimientodateTimePicker1.Value;
+            DateTime Antiguedad = TrabajodateTimePicker2.Value;
+            //Validacion de edad
+            String nombre = NombretextBox1.Text;
+            int edad = Edad(fechanacimien);
+            if(edad >= 30)
+            {
+                edadvalidacion = true;
+                MessageBox.Show("Usted tiene "+edad+" Años");
+
+            } else
+            {
+                edadvalidacion = false;
+                MessageBox.Show("Usted no cumple con los requisitos por que tiene " + edad + " Años");
+            }
+            //validacion de antiguedad
+            int Antiguo = AntiguedadCalculo(Antiguedad);
+
+            if (Antiguo >= 10)
+            {
+                Antiguedadvalidacion = true;
+                MessageBox.Show("Usted tiene " + Antiguo + " Años de laborar en la empresa\nSi aplica");
+
+            }
+            else
+            {
+                Antiguedadvalidacion = false;
+                MessageBox.Show("Usted no cumple con los requisitos por que tiene " + edad + " Años de laborar");
+            }
+            //validacion de ventas  
+            int sueldo = Convert.ToInt32(SueldotextBox1.Text);
+            int ventas = Convert.ToInt32(PromedioVentastextBox1.Text);
+
+            if (ventas >= 10000)
+            {
+                MessageBox.Show("Usted tiene " + ventas + " lempiras en ventas \nSi cumple este requisito");
+                ventasvalidacion = true;
+            }
+            else
+            {
+                MessageBox.Show("Usted tiene " + ventas + " lempiras en ventas \nNo cumple este requisito");
+                ventasvalidacion = false;
+            }
+
+            if (Antiguedadvalidacion == true && ventasvalidacion == true && edadvalidacion == true)
+            {
+                int aumento = 0;
+                aumento = Convert.ToInt32((sueldo + (sueldo * 0.05)));
+                
+                MessageBox.Show("Felicidades "+nombre+" su nuevo sueldo es de " + aumento + " lempiras \nSIGUE ASI!!!");
+            }
+            else
+            {
+                MessageBox.Show("Lo sentimos no aplicas para el aumento " + "\nNO TE DESANIMES!!! :D");
+            }
+
+
+        }
+        // funcion para calcular la edad del empleado 
+        private int Edad(DateTime fechanacimiento)
+        {
+            DateTime fechaactual = DateTime.Now;
+            int edad = 0;
+
+            if (fechanacimiento >= fechaactual)
+            {
+                MessageBox.Show("La edad ingresada no es valida \nPor favor intentelo de nuevo.");
+                return 0; 
+            }
+
+            edad = fechaactual.Year - fechanacimiento.Year;
+            if(fechanacimiento.Month > fechaactual.Month)
+            {
+                --edad;
+            }
+
+            return edad;
+        }
+        //funcion para obtener la cantidad de tiempo que el empleado a laborado en la empresa 
+        private int AntiguedadCalculo(DateTime fechAntiguedad)
+        {
+            DateTime fechaactual = DateTime.Now;
+            int antiguedad = 0;
+
+            if (fechAntiguedad >= fechaactual)
+            {
+                MessageBox.Show("¡¡ERROR!! \nLa fecha ingresada no es valida \nPor favor intentelo de nuevo.");
+                return 0;
+            }
+
+            antiguedad = fechaactual.Year - fechAntiguedad.Year;
+            if (fechAntiguedad.Month > fechaactual.Month)
+            {
+                --antiguedad;
+            }
+
+            return antiguedad;
+        }
+
     }
 }

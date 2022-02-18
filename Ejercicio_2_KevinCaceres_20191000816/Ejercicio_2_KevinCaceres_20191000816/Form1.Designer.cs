4704423ca62ed793c4ec854f657fd8abd04c6ae2
K-c-323 <99497355+K-c-323@users.noreply.github.com>
2022-02-18 11:40:16 -0600

    Ejercicio_2_KevinCaceres_20191000816
    Ejercicio 2 lenguaje de programacion

---
+++ Ejercicio_2_KevinCaceres_20191000816/Ejercicio_2_KevinCaceres_20191000816/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Ejercicio_2_KevinCaceres_20191000816
+{
+    partial class Ingresar_Nombres
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListadoComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.EdadTextBox = new System.Windows.Forms.TextBox();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.Limpiarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListadoComboBox
+            // 
+            this.ListadoComboBox.FormattingEnabled = true;
+            this.ListadoComboBox.Location = new System.Drawing.Point(273, 226);
+            this.ListadoComboBox.Name = "ListadoComboBox";
+            this.ListadoComboBox.Size = new System.Drawing.Size(246, 21);
+            this.ListadoComboBox.TabIndex = 0;
+            this.ListadoComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(387, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Edad : ";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(156, 87);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(207, 20);
+            this.NombreTextBox.TabIndex = 3;
+            // 
+            // EdadTextBox
+            // 
+            this.EdadTextBox.Location = new System.Drawing.Point(464, 90);
+            this.EdadTextBox.Name = "EdadTextBox";
+            this.EdadTextBox.Size = new System.Drawing.Size(117, 20);
+            this.EdadTextBox.TabIndex = 4;
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.AgregarButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AgregarButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AgregarButton.Location = new System.Drawing.Point(652, 75);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(113, 49);
+            this.AgregarButton.TabIndex = 5;
+            this.AgregarButton.Text = "Agregar";
+            this.AgregarButton.UseVisualStyleBackColor = false;
+            this.AgregarButton.Click += new System.EventHandler(this.AgregarButton_Click);
+            // 
+            // Limpiarbutton
+            // 
+            this.Limpiarbutton.Location = new System.Drawing.Point(669, 210);
+            this.Limpiarbutton.Name = "Limpiarbutton";
+            this.Limpiarbutton.Size = new System.Drawing.Size(119, 51);
+            this.Limpiarbutton.TabIndex = 6;
+            this.Limpiarbutton.Text = "Limpiar";
+            this.Limpiarbutton.UseVisualStyleBackColor = true;
+            this.Limpiarbutton.Click += new System.EventHandler(this.Limpiarbutton_Click);
+            // 
+            // Ingresar_Nombres
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Limpiarbutton);
+            this.Controls.Add(this.AgregarButton);
+            this.Controls.Add(this.EdadTextBox);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListadoComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Ingresar_Nombres";
+            this.Text = "Solicitar_Nombre_Edad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ListadoComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.TextBox EdadTextBox;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.Button Limpiarbutton;
+    }
+}
+
---
+++ Ejercicio_2_KevinCaceres_20191000816/Ejercicio_2_KevinCaceres_20191000816/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2_KevinCaceres_20191000816
+{
+    public partial class Ingresar_Nombres : Form
+    {
+        int contador = 0;
+        public Ingresar_Nombres()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AgregarButton_Click(object sender, EventArgs e)
+        {
+            
+            string[] nombres = new string[10];
+            int[] edad = new int[10];
+            int maximo = edad.Length;
+            if (contador < maximo)
+            {
+                nombres[contador] = NombreTextBox.Text;
+                edad[contador] = Convert.ToInt32(EdadTextBox.Text);
+                ListadoComboBox.Items.Add(nombres[contador] + " " + edad[contador]);
+                contador++;
+            }
+            else
+            {
+                MessageBox.Show("Lo sentimos haz Agregado el numero maximo de nombres","Save error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Limpiarbutton_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            ListadoComboBox.Items.Clear();
+
+        }
+    }
+}
